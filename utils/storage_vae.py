0f3e49f015e767a7177341b37c7f5b638d09dd10
Luisa Zintgraf <lmzintgraf@gmail.com>
2020-06-12 14:13:39 +0100

    reallocating vae storage instead of using lists

--- utils/storage_vae.py
+++ utils/storage_vae.py
@@ -40,12 +40,13 @@ class RolloutStorageVAE(object):
 
         # storage for each running process (stored on GPU)
         self.num_processes = num_processes
-        self.running_prev_obs = [[] for _ in range(num_processes)]  # for each episode will have obs 0...N-1
-        self.running_next_obs = [[] for _ in range(num_processes)]  # for each episode will have obs 1...N
-        self.running_rewards = [[] for _ in range(num_processes)]
-        self.running_actions = [[] for _ in range(num_processes)]
+        self.curr_timestep = torch.zeros((num_processes)).long()  # count environment steps so we know where to insert
+        self.running_prev_obs = torch.zeros((self.max_traj_len, num_processes, obs_dim)).to(device)  # for each episode will have obs 0...N-1
+        self.running_next_obs = torch.zeros((self.max_traj_len, num_processes, obs_dim)).to(device)  # for each episode will have obs 1...N
+        self.running_rewards = torch.zeros((self.max_traj_len, num_processes, rew_dim)).to(device)
+        self.running_actions = torch.zeros((self.max_traj_len, num_processes, action_dim)).to(device)
         if self.tasks is not None:
-            self.running_tasks = [None for _ in range(num_processes)]
+            self.running_tasks = torch.zeros((self.max_traj_len, num_processes)).to(device)
 
     def get_running_batch(self):
         """
@@ -53,102 +54,109 @@ class RolloutStorageVAE(object):
         (zero-padded to maximal trajectory length since different processes can have different trajectory lengths)
         :return:
         """
-        prev_obs = []
-        next_obs = []
-        actions = []
-        rewards = []
-        lengths = []
-        for i in range(self.num_processes):
-            pad_len = self.max_traj_len - len(self.running_prev_obs[i])
-            if pad_len > 0 and len(self.running_prev_obs[i]) > 0:
-                prev_obs.append(torch.cat((torch.cat(self.running_prev_obs[i]),
-                                           torch.zeros(pad_len, self.running_prev_obs[i][0].shape[1]).to(device))))
-                next_obs.append(torch.cat((torch.cat(self.running_next_obs[i]),
-                                           torch.zeros(pad_len, self.running_next_obs[i][0].shape[1]).to(device))))
-                actions.append(torch.cat((torch.cat(self.running_actions[i]),
-                                          torch.zeros(pad_len, self.running_actions[i][0].shape[1]).to(device))))
-                rewards.append(torch.cat((torch.cat(self.running_rewards[i]),
-                                          torch.zeros(pad_len, self.running_rewards[i][0].shape[1]).to(device))))
-            else:
-                prev_obs.append(torch.zeros(pad_len, self.obs_dim).to(device))
-                next_obs.append(torch.zeros(pad_len, self.obs_dim).to(device))
-                actions.append(torch.zeros(pad_len, self.action_dim).to(device))
-                rewards.append(torch.zeros(pad_len, self.rew_dim).to(device))
-            lengths.append(len(self.running_prev_obs[i]))
-        return torch.stack(prev_obs, dim=1), \
-               torch.stack(next_obs, dim=1), \
-               torch.stack(actions, dim=1), \
-               torch.stack(rewards, dim=1), \
-               lengths
+        return self.running_prev_obs, self.running_next_obs, self.running_actions, self.running_rewards, self.curr_timestep
 
     def insert(self, prev_obs, actions, next_obs, rewards, reset_task, task):
 
-        for i in range(self.num_processes):
-            self.running_prev_obs[i].append(prev_obs[i].unsqueeze(0))
-            self.running_next_obs[i].append(next_obs[i].unsqueeze(0))
-            self.running_rewards[i].append(rewards[i].unsqueeze(0))
-            self.running_actions[i].append(actions[i].unsqueeze(0).float())
+        already_inserted = False
+        if len(np.unique(self.curr_timestep)) == 1:
+            self.running_prev_obs[self.curr_timestep[0]] = prev_obs
+            self.running_next_obs[self.curr_timestep[0]] = next_obs
+            self.running_rewards[self.curr_timestep[0]] = rewards
+            self.running_actions[self.curr_timestep[0]] = actions
+            if task is not None:
+                self.running_tasks = task
+            self.curr_timestep += 1
+            already_inserted = True
 
-            if (self.tasks is not None) and (self.running_tasks[i] is None):
-                self.running_tasks[i] = task[i]
+        already_reset = False
+        if reset_task.sum() == self.num_processes:
+            # add to buffer
+            if self.vae_buffer_add_thresh >= np.random.uniform(0, 1):
+                # check where to insert data
+                if self.insert_idx + self.num_processes > self.max_buffer_size:
+                    # keep track of how much we filled the buffer (for sampling from it)
+                    self.buffer_len = self.insert_idx
+                    # this will keep some entries at the end of the buffer without overwriting them,
+                    # but the buffer is large enough to make this negligible
+                    self.insert_idx = 0
+                else:
+                    self.buffer_len = max(self.buffer_len, self.insert_idx)
 
-            # if we are at the end of a task, dump the data into the larger buffer
-            if reset_task[i]:
-
-                # add to buffer
-                if self.vae_buffer_add_thresh >= np.random.uniform(0, 1):
-
-                    # check where to insert data
-                    if self.insert_idx + 1 > self.max_buffer_size:
-                        # keep track of how much we filled the buffer (for sampling from it)
-                        self.buffer_len = self.insert_idx
-                        # this will keep some entries at the end of the buffer without overwriting them,
-                        # but the buffer is large enough to make this negligible
-                        self.insert_idx = 0
-                    else:
-                        self.buffer_len = max(self.buffer_len, self.insert_idx)
-
-                    # move data to CPU and bring into right shape
-                    self.running_prev_obs[i] = torch.cat(self.running_prev_obs[i]).to('cpu')
-                    self.running_next_obs[i] = torch.cat(self.running_next_obs[i]).to('cpu')
-                    self.running_actions[i] = torch.cat(self.running_actions[i]).to('cpu')
-                    self.running_rewards[i] = torch.cat(self.running_rewards[i]).to('cpu')
-                    if self.tasks is not None:
-                        self.running_tasks[i] = self.running_tasks[i].to('cpu')
-
-                    # pad with zeros to match maximum trajectory length
-                    if self.zero_pad:
-                        traj_len = len(self.running_prev_obs[i])  # length of the current trajectory
-                        pad_len = self.max_traj_len - traj_len  # how much we want to pad
-                        if pad_len > 0:
-                            self.running_prev_obs[i] = torch.cat(
-                                (self.running_prev_obs[i], torch.zeros(pad_len, self.running_prev_obs[i].shape[1])))
-                            self.running_next_obs[i] = torch.cat(
-                                (self.running_next_obs[i], torch.zeros(pad_len, self.running_next_obs[i].shape[1])))
-                            self.running_actions[i] = torch.cat(
-                                (self.running_actions[i], torch.zeros(pad_len, self.running_actions[i].shape[1])))
-                            self.running_rewards[i] = torch.cat(
-                                (self.running_rewards[i], torch.zeros(pad_len, self.running_rewards[i].shape[1])))
-
-                    # add; note: num trajectories are along dim=1,
-                    # trajectory length along dim=0, to match pytorch RNN interface
-                    self.prev_obs[:, self.insert_idx] = self.running_prev_obs[i]
-                    self.next_obs[:, self.insert_idx] = self.running_next_obs[i]
-                    self.actions[:, self.insert_idx] = self.running_actions[i]
-                    self.rewards[:, self.insert_idx] = self.running_rewards[i]
-                    if self.tasks is not None:
-                        self.tasks[self.insert_idx] = self.running_tasks[i]
-                    self.trajectory_lens[self.insert_idx] = len(self.running_prev_obs[i])
-
-                    self.insert_idx += 1
-
-                # empty running buffer
-                self.running_prev_obs[i] = []
-                self.running_next_obs[i] = []
-                self.running_rewards[i] = []
-                self.running_actions[i] = []
+                # add; note: num trajectories are along dim=1,
+                # trajectory length along dim=0, to match pytorch RNN interface
+                self.prev_obs[:, self.insert_idx:self.insert_idx+self.num_processes] = self.running_prev_obs
+                self.next_obs[:, self.insert_idx:self.insert_idx+self.num_processes] = self.running_next_obs
+                self.actions[:, self.insert_idx:self.insert_idx+self.num_processes] = self.running_actions
+                self.rewards[:, self.insert_idx:self.insert_idx+self.num_processes] = self.running_rewards
                 if self.tasks is not None:
-                    self.running_tasks[i] = None
+                    self.tasks[self.insert_idx:self.insert_idx+self.num_processes] = self.running_tasks
+                self.trajectory_lens[self.insert_idx:self.insert_idx+self.num_processes] = self.curr_timestep.clone()
+
+                self.insert_idx += self.num_processes
+
+            # empty running buffer
+            self.running_prev_obs *= 0
+            self.running_next_obs *= 0
+            self.running_rewards *= 0
+            self.running_actions *= 0
+            if self.tasks is not None:
+                self.running_tasks *= 0
+            self.curr_timestep *= 0
+
+            already_reset = True
+
+        if (not already_inserted) or (not already_reset):
+
+            for i in range(self.num_processes):
+
+                if not already_inserted:
+                    self.running_prev_obs[self.curr_timestep[i], i] = prev_obs[i]
+                    self.running_next_obs[self.curr_timestep[i], i] = next_obs[i]
+                    self.running_rewards[self.curr_timestep[i], i] = rewards[i]
+                    self.running_actions[self.curr_timestep[i], i] = actions[i]
+
+                    if (self.tasks is not None) and (self.running_tasks[i] is None):
+                        self.running_tasks[i] = task[i]
+                    self.curr_timestep[i] += 1
+
+                if not already_reset:
+                    # if we are at the end of a task, dump the data into the larger buffer
+                    if reset_task[i]:
+
+                        # add to buffer
+                        if self.vae_buffer_add_thresh >= np.random.uniform(0, 1):
+
+                            # check where to insert data
+                            if self.insert_idx + 1 > self.max_buffer_size:
+                                # keep track of how much we filled the buffer (for sampling from it)
+                                self.buffer_len = self.insert_idx
+                                # this will keep some entries at the end of the buffer without overwriting them,
+                                # but the buffer is large enough to make this negligible
+                                self.insert_idx = 0
+                            else:
+                                self.buffer_len = max(self.buffer_len, self.insert_idx)
+
+                            # add; note: num trajectories are along dim=1,
+                            # trajectory length along dim=0, to match pytorch RNN interface
+                            self.prev_obs[:, self.insert_idx] = self.running_prev_obs[:, i].to('cpu')
+                            self.next_obs[:, self.insert_idx] = self.running_next_obs[:, i].to('cpu')
+                            self.actions[:, self.insert_idx] = self.running_actions[:, i].to('cpu')
+                            self.rewards[:, self.insert_idx] = self.running_rewards[:, i].to('cpu')
+                            if self.tasks is not None:
+                                self.tasks[self.insert_idx] = self.running_tasks[i].to('cpu')
+                            self.trajectory_lens[self.insert_idx] = self.curr_timestep[i].clone()
+
+                            self.insert_idx += 1
+
+                        # empty running buffer
+                        self.running_prev_obs[:, i] *= 0
+                        self.running_next_obs[:, i] *= 0
+                        self.running_rewards[:, i] *= 0
+                        self.running_actions[:, i] *= 0
+                        if self.tasks is not None:
+                            self.running_tasks[i] *= 0
+                        self.curr_timestep[i] = 0
 
     def ready_for_update(self):
         return len(self) > 0
