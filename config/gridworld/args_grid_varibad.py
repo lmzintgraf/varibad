fix categorical/softmax prediction

config/gridworld/args_grid_varibad.py
@@ -104,7 +104,7 @@ def get_args(rest_args):
     parser.add_argument('--rew_pred_type', type=str, default='bernoulli',
                         help='choose: '
                              'bernoulli (predict p(r=1|s))'
-                             'categorical (treat as classification problem, r=1 for *one* state)'
+                             'categorical (predict p(r=1|s) but use softmax instead of sigmoid)'
                              'deterministic (treat as regression problem)')
 
     # - decoder: state transitions
@@ -162,6 +162,14 @@ def get_args(rest_args):
     parser.add_argument('--split_batches_by_elbo', type=bool, default=False, help='split batches up by elbo term (to save memory)')
     parser.add_argument('--port', type=int, default=8097, help='port to run the server on (default: 8097)')
     args = parser.parse_args(rest_args)
+
+    if args.disable_decoder:
+        args.decode_reward = False
+        args.decode_state = False
+        args.decode_task = False
+    
+    if args.decode_only_past:
+        args.split_batches_by_elbo = True
 
     args.cuda = torch.cuda.is_available()
 

vae.py
@@ -154,19 +154,23 @@ class VaribadVAE:
         (No reduction of loss along batch dimension is done here; sum/avg has to be done outside) """
 
         if self.args.multihead_for_reward:
-            p_rew = self.reward_decoder(latent, None)
+
+            rew_pred = self.reward_decoder(latent, None)
+            if self.args.rew_pred_type == 'categorical':
+                rew_pred = F.softmax(rew_pred)
+            elif self.args.rew_pred_type == 'bernoulli':
+                rew_pred = F.sigmoid(rew_pred)
+
             env = gym.make(self.args.env_name)
-            indices = env.task_to_id(next_obs).to(device)
-            if indices.dim() < p_rew.dim():
-                indices = indices.unsqueeze(-1)
-            rew_pred = p_rew.gather(dim=-1, index=indices)
+            state_indices = env.task_to_id(next_obs).to(device)
+            if state_indices.dim() < rew_pred.dim():
+                state_indices = state_indices.unsqueeze(-1)
+            rew_pred = rew_pred.gather(dim=-1, index=state_indices)
             rew_target = (reward == 1).float()
-            if self.args.rew_pred_type == 'bernoulli':
-                loss_rew = F.binary_cross_entropy_with_logits(rew_pred, rew_target, reduction='none').mean(dim=-1)
-            elif self.args.rew_pred_type == 'categorical':
-                loss_rew = F.cross_entropy(rew_pred, rew_target, reduction='none').mean(dim=-1)
-            elif self.args.rew_pred_type == 'deterministic':  # TODO: untested!
+            if self.args.rew_pred_type == 'deterministic':  # TODO: untested!
                 loss_rew = (rew_pred - reward).pow(2).mean(dim=-1)
+            elif self.args.rew_pred_type in ['categorical', 'bernoulli']:
+                loss_rew = F.binary_cross_entropy(rew_pred, rew_target, reduction='none').mean(dim=-1)
             else:
                 raise NotImplementedError
         else: