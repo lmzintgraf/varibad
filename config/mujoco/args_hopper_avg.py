a3dd541836f75a5aacdd07324f8eb3b89723ba5b
Luisa Zintgraf <lmzintgraf@gmail.com>
2020-08-24 16:45:58 +0100

    1e8 steps for all mujoco envs

--- config/mujoco/args_hopper_avg.py
+++ config/mujoco/args_hopper_avg.py
@@ -7,7 +7,7 @@ def get_args(rest_args):
 
     # --- GENERAL ---
 
-    parser.add_argument('--num_frames', type=int, default=5e7, help='number of frames to train')
+    parser.add_argument('--num_frames', type=int, default=1e8, help='number of frames to train')
     parser.add_argument('--max_rollouts_per_task', type=int, default=1)
     parser.add_argument('--exp_label', default='oracle', help='label for the experiment')
     parser.add_argument('--env_name', default='HopperRandParams-v0', help='environment to train on')
@@ -49,13 +49,13 @@ def get_args(rest_args):
 
     # PPO specific
     parser.add_argument('--ppo_num_epochs', type=int, default=2, help='number of epochs per PPO update')
-    parser.add_argument('--ppo_num_minibatch', type=int, default=4, help='number of minibatches to split the data')
+    parser.add_argument('--ppo_num_minibatch', type=int, default=2, help='number of minibatches to split the data')
     parser.add_argument('--ppo_use_huberloss', type=boolean_argument, default=True, help='use huberloss instead of MSE')
     parser.add_argument('--ppo_use_clipped_value_loss', type=boolean_argument, default=True, help='clip value loss')
     parser.add_argument('--ppo_clip_param', type=float, default=0.1, help='clamp param')
 
     # other hyperparameters
-    parser.add_argument('--lr_policy', type=float, default=7e-4, help='learning rate (default: 7e-4)')
+    parser.add_argument('--lr_policy', type=float, default=1e-3, help='learning rate (default: 7e-4)')
     parser.add_argument('--num_processes', type=int, default=16,
                         help='how many training CPU processes / parallel environments to use (default: 16)')
     parser.add_argument('--policy_num_steps', type=int, default=200,
